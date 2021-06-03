aadb899df20d583b344b98cdb98bb1d77968c36a
Ryder <ryderblue2@gmail.com>
2021-06-03 10:22:56 -0700

    wip3

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require("express");
+const server = express();
+
+const PORT = process.env.PORT || 5000;
+
+server.get('/api', (req, res) => {
+    res.json({message: `${process.env.COHORT} rocks` })
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
+console.log(process.env)
